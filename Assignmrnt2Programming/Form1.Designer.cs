7ad15619271339066ab218f7a9483082584d262f
RC0033225 <RC0033225@CRO-24-03>
2018-10-16 10:27:04 +0100

    I have made the drop down boxes work.

--- Assignmrnt2Programming/Form1.Designer.cs
+++ Assignmrnt2Programming/Form1.Designer.cs
@@ -28,13 +28,371 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Parallel = new System.Windows.Forms.TabPage();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Series = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Answer1 = new System.Windows.Forms.Label();
+            this.Answer = new System.Windows.Forms.Label();
+            this.Parallel.SuspendLayout();
+            this.Series.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Parallel
+            // 
+            this.Parallel.Controls.Add(this.Answer);
+            this.Parallel.Controls.Add(this.button2);
+            this.Parallel.Controls.Add(this.label10);
+            this.Parallel.Controls.Add(this.label9);
+            this.Parallel.Controls.Add(this.label8);
+            this.Parallel.Controls.Add(this.label7);
+            this.Parallel.Controls.Add(this.label6);
+            this.Parallel.Controls.Add(this.textBox10);
+            this.Parallel.Controls.Add(this.textBox9);
+            this.Parallel.Controls.Add(this.textBox8);
+            this.Parallel.Controls.Add(this.textBox7);
+            this.Parallel.Controls.Add(this.textBox6);
+            this.Parallel.Controls.Add(this.comboBox2);
+            this.Parallel.Location = new System.Drawing.Point(4, 22);
+            this.Parallel.Name = "Parallel";
+            this.Parallel.Padding = new System.Windows.Forms.Padding(3);
+            this.Parallel.Size = new System.Drawing.Size(792, 424);
+            this.Parallel.TabIndex = 1;
+            this.Parallel.Text = "Parallel";
+            this.Parallel.UseVisualStyleBackColor = true;
+            this.Parallel.Click += new System.EventHandler(this.Parallel_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "2 Resistors",
+            "3 Resistors",
+            "4 Resistors",
+            "5 Resistors"});
+            this.comboBox2.Location = new System.Drawing.Point(8, 37);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 0;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(202, 52);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 1;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(202, 92);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 2;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(202, 132);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 3;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(202, 176);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 4;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(202, 218);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(175, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "R1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(175, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "R2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(175, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "R3";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(175, 183);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "R4";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(175, 225);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "R5";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(588, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Series
+            // 
+            this.Series.Controls.Add(this.Answer1);
+            this.Series.Controls.Add(this.button1);
+            this.Series.Controls.Add(this.comboBox1);
+            this.Series.Controls.Add(this.label5);
+            this.Series.Controls.Add(this.label4);
+            this.Series.Controls.Add(this.label3);
+            this.Series.Controls.Add(this.label2);
+            this.Series.Controls.Add(this.label1);
+            this.Series.Controls.Add(this.textBox5);
+            this.Series.Controls.Add(this.textBox4);
+            this.Series.Controls.Add(this.textBox3);
+            this.Series.Controls.Add(this.textBox2);
+            this.Series.Controls.Add(this.textBox1);
+            this.Series.Location = new System.Drawing.Point(4, 22);
+            this.Series.Name = "Series";
+            this.Series.Padding = new System.Windows.Forms.Padding(3);
+            this.Series.Size = new System.Drawing.Size(792, 424);
+            this.Series.TabIndex = 0;
+            this.Series.Text = "Series";
+            this.Series.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(243, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(505, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(243, 145);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(505, 145);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(378, 197);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "R1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(478, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "R2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(216, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "R3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(478, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "R4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(351, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "R5";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2 Resistors",
+            "3 Resistors",
+            "4 Resistors",
+            "5 Resistors"});
+            this.comboBox1.Location = new System.Drawing.Point(8, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(604, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Series);
+            this.tabControl1.Controls.Add(this.Parallel);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Answer1
+            // 
+            this.Answer1.AutoSize = true;
+            this.Answer1.Location = new System.Drawing.Point(601, 225);
+            this.Answer1.Name = "Answer1";
+            this.Answer1.Size = new System.Drawing.Size(42, 13);
+            this.Answer1.TabIndex = 12;
+            this.Answer1.Text = "Answer";
+            // 
+            // Answer
+            // 
+            this.Answer.AutoSize = true;
+            this.Answer.Location = new System.Drawing.Point(585, 218);
+            this.Answer.Name = "Answer";
+            this.Answer.Size = new System.Drawing.Size(42, 13);
+            this.Answer.TabIndex = 12;
+            this.Answer.Text = "Answer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Parallel.ResumeLayout(false);
+            this.Parallel.PerformLayout();
+            this.Series.ResumeLayout(false);
+            this.Series.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabPage Parallel;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TabPage Series;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Label Answer;
+        private System.Windows.Forms.Label Answer1;
     }
 }
 
--- Assignmrnt2Programming/Form1.cs
+++ Assignmrnt2Programming/Form1.cs
@@ -16,5 +16,99 @@ namespace Assignmrnt2Programming
         {
             InitializeComponent();
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                label3.Hide();
+                textBox3.Hide();
+                label4.Hide();
+                textBox4.Hide();
+                label5.Hide();
+                textBox5.Hide();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                label4.Hide();
+                textBox4.Hide();
+                label5.Hide();
+                textBox5.Hide();
+
+                label3.Show();
+                textBox3.Show();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                label5.Hide();
+                textBox5.Hide();
+
+                label3.Show();
+                textBox3.Show();
+                label4.Show();
+                textBox4.Show();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                label3.Show();
+                textBox3.Show();
+                label4.Show();
+                textBox4.Show();
+                label5.Show();
+                textBox5.Show();
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex == 0)
+            {
+                label8.Hide();
+                textBox8.Hide();
+                label9.Hide();
+                textBox9.Hide();
+                label10.Hide();
+                textBox10.Hide();
+            }
+            else if (comboBox2.SelectedIndex == 1)
+            {
+                label9.Hide();
+                textBox9.Hide();
+                label10.Hide();
+                textBox10.Hide();
+
+                label8.Show();
+                textBox8.Show();
+            }
+            else if (comboBox2.SelectedIndex == 2)
+            {
+                label10.Hide();
+                textBox10.Hide();
+
+                label8.Show();
+                textBox8.Show();
+                label9.Show();
+                textBox9.Show();
+            }
+            else if (comboBox2.SelectedIndex == 3)
+            {
+                label8.Show();
+                textBox8.Show();
+                label9.Show();
+                textBox9.Show();
+                label10.Show();
+                textBox10.Show();
+            }
+        }
+
+        private void Parallel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
